e8ecdcd946b024fa2f53e4e5cb4577d3e20a8d32
GalvezTerrerosMauro <maurogaaalvezterreros@outlook.es>
2020-11-06 14:14:13 -0500

    docente

---
+++ Profesor/app/src/main/java/edu/pe/continental/vadt/teacher/CursosRapidosActivity.java
@@ -0,0 +1,14 @@
+package edu.pe.continental.vadt.teacher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CursosRapidosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cursos_rapidos);
+    }
+}
