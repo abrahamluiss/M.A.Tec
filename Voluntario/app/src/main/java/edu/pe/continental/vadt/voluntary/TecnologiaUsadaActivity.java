cd789dff770267c1576f25db810a02e18a064e15
Cristhian <43081931+Leonardo9513@users.noreply.github.com>
2020-11-05 22:50:52 -0500

    Tecnologia

---
+++ Voluntario/app/src/main/java/edu/pe/continental/vadt/voluntary/TecnologiaUsadaActivity.java
@@ -0,0 +1,14 @@
+package edu.pe.continental.vadt.voluntary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TecnologiaUsadaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tecnologia_usada);
+    }
+}
