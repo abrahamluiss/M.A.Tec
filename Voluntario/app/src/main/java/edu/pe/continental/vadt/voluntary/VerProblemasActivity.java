718292c441ad31ffd5894d97284eb074f4d344c7
GalvezTerrerosMauro <maurogaaalvezterreros@outlook.es>
2020-11-06 11:48:54 -0500

    layotus voluntario

---
+++ Voluntario/app/src/main/java/edu/pe/continental/vadt/voluntary/VerProblemasActivity.java
@@ -0,0 +1,14 @@
+package edu.pe.continental.vadt.voluntary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VerProblemasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_problemas);
+    }
+}
