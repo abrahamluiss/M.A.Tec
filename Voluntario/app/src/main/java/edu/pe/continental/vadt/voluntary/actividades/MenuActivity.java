6a323b1da6180bde764451b468a347475498d99e
Cristhian <43081931+Leonardo9513@users.noreply.github.com>
2020-11-07 00:02:55 -0500

    Actividades01

--- Voluntario/app/src/main/java/edu/pe/continental/vadt/voluntary/actividades/MenuActivity.java
+++ Voluntario/app/src/main/java/edu/pe/continental/vadt/voluntary/actividades/MenuActivity.java
@@ -11,16 +11,53 @@ import edu.pe.continental.vadt.voluntary.R;
 
 public class MenuActivity extends AppCompatActivity {
 
-    CardView mCarRegitrar;
+    CardView mCarRegitrar,mCarCurso,mCarValorar,mCarTecno,mCarRuta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         mCarRegitrar = findViewById(R.id.carRegitrar);
+        mCarCurso = findViewById(R.id.carCurso);
+        mCarValorar = findViewById(R.id.carValoracion);
+        mCarTecno = findViewById(R.id.carTecnologia);
+        mCarRuta = findViewById(R.id.carRutapre);
+
         mCarRegitrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MenuActivity.this, CentroProblemasActivity.class);
+                //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+        mCarCurso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, RegistrarCursoActivity.class);
+                //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+        mCarValorar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, ComentariosActivity.class);
+                //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+        mCarTecno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, TecnologiaUsadaActivity.class);
+                //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+        mCarRuta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, MenuRutaActivity.class);
                 //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
             }
