8b080c3c23f5bfdcaac92ab18f7fee6cbd7bb288
Cristhian <43081931+Leonardo9513@users.noreply.github.com>
2020-11-06 23:29:11 -0500

    Actividades

--- Voluntario/app/src/main/java/edu/pe/continental/vadt/voluntary/RutaAprendisaje/InicialActivity.java
+++ Voluntario/app/src/main/java/edu/pe/continental/vadt/voluntary/RutaAprendisaje/InicialActivity.java
@@ -2,16 +2,70 @@
 package edu.pe.continental.vadt.voluntary.RutaAprendisaje;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import edu.pe.continental.vadt.voluntary.R;
 
 public class InicialActivity extends AppCompatActivity {
+    CardView car01,car02,car03,car04, car05, car06;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicial);
+
+        car01.findViewById(R.id.carVideoClase);
+        car02.findViewById(R.id.carVideoClase);
+        car03.findViewById(R.id.carVideoClase);
+        car04.findViewById(R.id.carVideoClase);
+        car05.findViewById(R.id.carVideoClase);
+        car06.findViewById(R.id.carVideoClase);
+
+        car01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InicialActivity.this, VideoActivity.class);
+                startActivity(intent);
+            }
+        });
+        car02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InicialActivity.this, VideoActivity.class);
+                startActivity(intent);
+            }
+        });
+        car03.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InicialActivity.this, VideoActivity.class);
+                startActivity(intent);
+            }
+        });
+        car04.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InicialActivity.this, VideoActivity.class);
+                startActivity(intent);
+            }
+        });
+        car05.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InicialActivity.this, VideoActivity.class);
+                startActivity(intent);
+            }
+        });
+        car06.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InicialActivity.this, VideoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- Voluntario/app/src/main/java/edu/pe/continental/vadt/voluntary/RutaAprendisaje/MenuRutaActivity.java
+++ Voluntario/app/src/main/java/edu/pe/continental/vadt/voluntary/RutaAprendisaje/MenuRutaActivity.java
@@ -4,7 +4,10 @@ package edu.pe.continental.vadt.voluntary.RutaAprendisaje;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import edu.pe.continental.vadt.voluntary.R;
 
@@ -14,5 +17,22 @@ public class MenuRutaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_ruta);
+    }
+    public void onClick(View view){
+        Intent intent=null;
+        switch (view.getId()){
+            case R.id.btnInicial:
+                intent = new Intent(MenuRutaActivity.this, InicialActivity.class);
+                break;
+            case R.id.btnPrimari:
+                intent = new Intent(MenuRutaActivity.this, PrimariActivity.class);
+                break;
+            case R.id.btnSecund:
+                intent = new Intent(MenuRutaActivity.this, SecundariActivity.class);
+            case R.id.btnSuperior:
+                intent = new Intent(MenuRutaActivity.this, SuperiActivity.class);
+                break;
+        }
+        startActivity(intent);
     }
 }
